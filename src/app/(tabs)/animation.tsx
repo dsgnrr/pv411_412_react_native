fbc6a442ac57ab6a298b51299c73d80faa7ae4bd
Semeniuk Oleg <semeniuk.oleg2002@gmail.com>
2026-07-13 22:26:30 +0300

    +додаткові анімації

--- src/app/(tabs)/animation.tsx
+++ src/app/(tabs)/animation.tsx
@@ -5,8 +5,15 @@ import { Container } from "@/components/ui/container";
 const AnimateScreen = () => {
 
     const fadeAnim = useRef(new Animated.Value(0)).current;
+
+    // Анімація руху блока
     const moveAnim = useRef(new Animated.Value(0)).current;
+
+    // Анімація кольору блока
     const colorAnim = useRef(new Animated.Value(0)).current;
+
+    // Анімація рамки у Fade animation
+    const fadeBoxColorAnim = useRef(new Animated.Value(0)).current;
 
     const animate = (toValue: number, animateValue: Animated.Value, duration = 3000, useNativeDriver = true) => {
         Animated.timing(animateValue, {
@@ -16,21 +23,31 @@ const AnimateScreen = () => {
         }).start()
     }
 
+    // інтерполяція для кольору блока
     const boxColor = colorAnim.interpolate({
         inputRange: [0, 1],
         outputRange: ['#1f1f1f', '#fff']
     })
 
-     const moveInterpol = moveAnim.interpolate({
+    // інтерполяція для кольору рамки
+    const borderColor = fadeBoxColorAnim.interpolate({
+        inputRange: [0, 1],
+        outputRange: ['#1f1f1f', '#fff']
+    })
+
+    // інтерполяція руху блока
+    const moveInterpol = moveAnim.interpolate({
         inputRange: [0, 1],
         outputRange: ["0%", "100%"]
     })
 
     const show = () => {
         animate(1, fadeAnim, 3000)
+        animate(1, fadeBoxColorAnim, 3000, false)
     }
     const suspense = () => {
         animate(0, fadeAnim, 3000)
+        animate(0, fadeBoxColorAnim, 3000, false)
     }
 
     /* Анімація не спрацювала по тій причині, що анімації які керуються нейтів-двигуном, а анімації не керуються
@@ -41,8 +58,17 @@ const AnimateScreen = () => {
         <ScrollView>
             <Container>
                 <Text style={styles.headerText}>Fade animation</Text>
-                <Animated.View style={[styles.fadeBox, { opacity: fadeAnim }]}>
-                    <Text style={styles.text}>Animate</Text>
+                {/* Анімована рамка, з'являєтся коли зникає блок */}
+                <Animated.View style={[styles.fadeTextContainer, {
+                    borderColor: borderColor
+                }]}>
+                    {/* Анімована блок, зникає при натисканні на кнопку Suspense */}
+                    <Animated.View style={[
+                        StyleSheet.absoluteFill,
+                        styles.fadeBox,
+                        { opacity: fadeAnim }
+                    ]} />
+                    <Text style={styles.text}>stealth mode</Text>
                 </Animated.View>
                 <View style={{ flexDirection: 'row', gap: 3 }}>
                     <TouchableOpacity onPress={() => show()} style={[styles.buttonBase, { backgroundColor: "#a03064", marginVertical: 5 }]}>
@@ -64,14 +90,14 @@ const AnimateScreen = () => {
                         }],
                         backgroundColor: boxColor
                     }]}>
-                        <Text
+                    <Text
                         style={{
-                            color:"#1f1f1f",
-                            textTransform:'uppercase',
+                            color: "#1f1f1f",
+                            textTransform: 'uppercase',
                             fontSize: 16,
-                            fontWeight:'bold'
-                        }}>Hello</Text>
-                    </Animated.View>
+                            fontWeight: 'bold'
+                        }}>i'm here</Text>
+                </Animated.View>
                 <View style={{ flexDirection: 'row', gap: 3 }}>
                     <TouchableOpacity onPress={() => {
                         animate(1, moveAnim, 3000, false)
@@ -109,9 +135,8 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
     fadeBox: {
-        padding: 20,
         backgroundColor: "#1f1f1f",
-        borderRadius: 8
+        borderRadius: 6
     },
     buttonBase: {
         flex: 1,
@@ -125,9 +150,18 @@ const styles = StyleSheet.create({
         textTransform: "uppercase",
         fontWeight: 'bold'
     },
+    fadeTextContainer: {
+        height: 50,
+        justifyContent: 'center',
+        borderRadius: 8,
+        borderColor: "#1f1f1f",
+        borderWidth: 2,
+    },
     text: {
-        color: "#fff",
-        fontSize: 20
+        color: "#1f1f1f",
+        fontSize: 20,
+        textAlign: 'center',
+        textTransform: "uppercase"
     }
 })
 
